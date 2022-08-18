d904f0dd36505a14b432d68dac3111ec621095d6
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2022-08-18 19:29:18 +0200

    error handler

--- src/Handlers/HttpErrorHandler.php
+++ src/Handlers/HttpErrorHandler.php
@@ -4,6 +4,7 @@ namespace ACAT\Slim\Handlers;
 
 use ACAT\Slim\Actions\ActionError;
 use ACAT\Slim\Actions\ActionPayload;
+use Psr\Http\Message\ResponseInterface;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpException;
 use Slim\Exception\HttpForbiddenException;
@@ -23,49 +24,14 @@ class HttpErrorHandler extends ErrorHandler {
     /**
      * @inheritdoc
      */
-    protected function respond(): Response {
+    protected function respond(): ResponseInterface {
 
-        $exception = $this->exception;
-        $statusCode = 500;
-        $error = new ActionError(ActionError::SERVER_ERROR, 'An internal error has occurred while processing your request.');
-
-        if ($exception instanceof HttpException) {
-
-            $statusCode = $exception->getCode();
-            $error->setDescription($exception->getMessage());
-
-            if ($exception instanceof HttpNotFoundException) {
-                $error->setType(ActionError::RESOURCE_NOT_FOUND);
-            }
-            elseif ($exception instanceof HttpMethodNotAllowedException) {
-                $error->setType(ActionError::NOT_ALLOWED);
-            }
-            elseif ($exception instanceof HttpUnauthorizedException) {
-                $error->setType(ActionError::UNAUTHENTICATED);
-            }
-            elseif ($exception instanceof HttpForbiddenException) {
-                $error->setType(ActionError::INSUFFICIENT_PRIVILEGES);
-            }
-            elseif ($exception instanceof HttpBadRequestException) {
-                $error->setType(ActionError::BAD_REQUEST);
-            }
-            elseif ($exception instanceof HttpNotImplementedException) {
-                $error->setType(ActionError::NOT_IMPLEMENTED);
-            }
-
+        if (!$this->exception instanceof  HttpNotFoundException) {
+            $this->logger->critical($this->exception->getMessage());
         }
 
-        if (!($exception instanceof HttpException) && $exception instanceof Throwable && $this->displayErrorDetails) {
-            $error->setDescription($exception->getMessage());
-        }
-
-        $payload = new ActionPayload($statusCode, null, $error);
-        $encodedPayload = json_encode($payload, JSON_PRETTY_PRINT);
-
-        $response = $this->responseFactory->createResponse($statusCode);
-        $response->getBody()->write($encodedPayload);
-
-        return $response->withHeader('Content-Type', 'application/json');
+        return $this->responseFactory->createResponse($this->exception->getCode());
 
     }
+
 }
