b0127f3b3c7b0f65c93c7d7893d15a1e0178a0a1
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2025-01-24 11:54:56 +0100

    added origins

--- src/Middleware/CORS.php
+++ src/Middleware/CORS.php
@@ -2,7 +2,8 @@
 
 namespace ACAT\Slim\Middleware;
 
-use Slim\Psr7\Response;
+use Psr\Log\LoggerInterface;
+use Slim\Psr7\Factory\ResponseFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,37 +12,70 @@ use Psr\Http\Server\RequestHandlerInterface;
 /**
  *
  */
-final class CORS implements MiddlewareInterface
-{
+final class CORS implements MiddlewareInterface {
 
-    /**
-     * @param   ServerRequestInterface   $request
-     * @param   RequestHandlerInterface  $handler
-     *
-     * @return ResponseInterface
-     */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
-    {
-        if ($request->getMethod() === 'OPTIONS') {
-            return $this->createCORSHeader(new Response())->withStatus(204);
-        }
+	/**
+	 * @var array
+	 */
+	private array $allowedOrigins;
 
-        $response = $handler->handle($request);
+	/**
+	 * @var LoggerInterface
+	 */
+	private LoggerInterface $logger;
 
-        return $this->createCORSHeader($response);
-    }
+	/**
+	 * @var ResponseFactory
+	 */
+	private ResponseFactory $responseFactory;
 
-    /**
-     * @param   ResponseInterface  $response
-     *
-     * @return ResponseInterface
-     */
-    private function createCORSHeader(ResponseInterface $response) : ResponseInterface
-    {
-        return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
-            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	/**
+	 * @param   LoggerInterface  $logger
+	 * @param   array            $allowedOrigins
+	 * @param   ResponseFactory  $responseFactory
+	 */
+	public function __construct(LoggerInterface $logger, array $allowedOrigins, ResponseFactory $responseFactory) {
+		$this->logger = $logger;
+		$this->allowedOrigins = $allowedOrigins;
+		$this->responseFactory = $responseFactory;
+	}
+
+	/**
+	 * @param   ServerRequestInterface   $request
+	 * @param   RequestHandlerInterface  $handler
+	 *
+	 * @return ResponseInterface
+	 */
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface {
+
+		$origin = $request->getHeaderLine('Origin');
+
+		if ( ! $origin || ! in_array($origin, $this->allowedOrigins)) {
+			$this->logger->warning('origin ' . ($origin ?: 'unknown') . ' not set or allowed');
+			return $this->responseFactory->createResponse(403);
+		}
+
+		if ($request->getMethod() === 'OPTIONS') {
+			return $this->createCORSHeader($this->responseFactory->createResponse(204), $origin);
+		}
+
+		$response = $handler->handle($request);
+		return $this->createCORSHeader($response, $origin);
+
+	}
+
+	/**
+	 * @param   ResponseInterface  $response
+	 * @param   string             $origin
+	 *
+	 * @return ResponseInterface
+	 */
+	private function createCORSHeader(ResponseInterface $response, string $origin) : ResponseInterface {
+		return $response
+			->withHeader('Access-Control-Allow-Origin', $origin)
+			->withHeader('Access-Control-Allow-Credentials', 'true')
+			->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
+			->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 
 }
