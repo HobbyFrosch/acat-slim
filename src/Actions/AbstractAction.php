f88f219519039391abdd62fb2c968dd398dccdba
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2025-01-25 13:01:08 +0100

    parameter is optinal now

--- src/Actions/AbstractAction.php
+++ src/Actions/AbstractAction.php
@@ -11,47 +11,44 @@ use Psr\Http\Message\ResponseInterface as Response;
 /**
  *
  */
-abstract class AbstractAction extends Action
-{
+abstract class AbstractAction extends Action {
 
-    /**
-     * @var ServiceInterface
-     */
-    protected ServiceInterface $serviceInterface;
+	/**
+	 * @var ServiceInterface|null
+	 */
+	protected ?ServiceInterface $serviceInterface;
 
-    /**
-     * @param   ServiceInterface  $serviceInterface
-     * @param   LoggerInterface   $logger
-     */
-    public function __construct(ServiceInterface $serviceInterface, LoggerInterface $logger)
-    {
-        parent::__construct($logger);
-        $this->serviceInterface = $serviceInterface;
-    }
+	/**
+	 * @param   LoggerInterface        $logger
+	 * @param   ServiceInterface|null  $serviceInterface
+	 */
+	public function __construct(LoggerInterface $logger, ?ServiceInterface $serviceInterface = null) {
+		parent::__construct($logger);
+		$this->serviceInterface = $serviceInterface;
+	}
 
-    /**
-     * @return Response
-     */
-    protected function action() : Response
-    {
-        try {
-            return $this->process();
-        }
-        catch (Exception $e) {
-            $this->logger->critical($e);
-            if ( ! $e->getCode()) {
-                $code = 500;
-            } else {
-                $code = $e->getCode();
-            }
+	/**
+	 * @return Response
+	 */
+	protected function action() : Response {
+		try {
+			return $this->process();
+		}
+		catch (Exception $e) {
+			$this->logger->critical($e);
+			if ( ! $e->getCode()) {
+				$code = 500;
+			}
+			else {
+				$code = $e->getCode();
+			}
+			return $this->response->withStatus($code);
+		}
+	}
 
-            return $this->response->withStatus($code);
-        }
-    }
-
-    /**
-     * @return Response
-     */
-    abstract protected function process() : Response;
+	/**
+	 * @return Response
+	 */
+	abstract protected function process() : Response;
 
 }
